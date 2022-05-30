c56aa7555514fd8b8187aa5fbc1d769699912e2d
Sravan <sravankomati.vision@gmail.com>
2022-05-30 12:53:39 +0530

    adding cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 require("./config/db");
 const mainroutes = require("./routes/index");
+const cors = require("cors");
 const port = process.env.PORT || 4001;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
