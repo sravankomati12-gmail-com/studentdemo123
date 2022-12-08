ace88d074c1f94258affa27bb91585793b2718e5
sravanvision123 <106365633+sravanvision123@users.noreply.github.com>
2022-12-08 17:04:37 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://sravan_vision:Sravan%40123@cluster0.vr3xu.mongodb.net/studentdb?retryWrites=true&w=majority",
+  "mongodb+srv://sravan_vision:sravan@cluster0.vr3xu.mongodb.net/studentdb?retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 mongoose.connection.on("error", function (error) {
